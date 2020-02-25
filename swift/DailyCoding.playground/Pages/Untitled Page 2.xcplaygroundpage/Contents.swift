8b18d82aeaccbcec68d5a12828dc1c1f42d93ddc
newoo <45201399+newoo@users.noreply.github.com>
2020-02-25 18:56:10 +0900

    Move folder of playground file
    Make swift folder
    Playground file move to swift folder

---
+++ swift/DailyCoding.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+
+import Foundation
+import XCTest
+
+
+
+class getTests: XCTestCase {
+    func testSolution() {
+        XCTAssertEqual(1, 1)
+    }
+}
+
+getTests.defaultTestSuite.run()
+
+//: [Next](@next)
