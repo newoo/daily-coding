7070d5c5001173c5bea4400edb80bddaadf97a08
newoo <newoo4297@naver.com>
2022-02-05 16:00:43 +0900

    Solve open-chat-room by swift

--- swift/open-chat-room.swift
+++ swift/open-chat-room.swift
@@ -1,5 +1,3 @@
-//: [Previous](@previous)
-
 import Foundation
 
 /**
@@ -44,4 +42,3 @@ struct Msg {
 
 print(solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"])) // ["Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."]
 
-//: [Next](@next)
