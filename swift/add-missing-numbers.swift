d3aaf12ffb5a8a0dc7904da10d2e702eb34ff528
newoo <newoo4297@naver.com>
2022-02-09 21:28:45 +0900

    Solve add-missing-numbers by swift

--- swift/add-missing-numbers.swift
+++ swift/add-missing-numbers.swift
@@ -1,5 +1,3 @@
-//: [Previous](@previous)
-
 // https://programmers.co.kr/learn/courses/30/lessons/86051
 
 import Foundation
@@ -13,4 +11,3 @@ func solution(_ numbers:[Int]) -> Int {
 print(solution([1,2,3,4,6,7,8,0])) // 14
 print(solution([5,8,4,0,6,7,9])) // 6
 
-//: [Next](@next)
