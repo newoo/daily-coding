37aac2b828a0fae8a1cc641a247dd3b06eb625e0
newoo <newoo4297@naver.com>
2022-02-11 22:31:36 +0900

    Solve minimum-rectangle by swift

--- swift/minimum-rectangle.swift
+++ swift/minimum-rectangle.swift
@@ -1,5 +1,3 @@
-//: [Previous](@previous)
-
 // https://programmers.co.kr/learn/courses/30/lessons/86491
 
 import Foundation
@@ -20,4 +18,3 @@ print(solution([[60, 50], [30, 70], [60, 30], [80, 40]])) // 4000
 print(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]])) // 120
 print(solution([[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]])) // 133
 
-//: [Next](@next)
