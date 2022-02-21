ff906a0a8fd0a1ccf63d20d0c338a18b8bf83d9d
newoo <newoo4297@naver.com>
2022-02-21 16:15:02 +0900

    Solve add-two-numbers by swift

--- swift/add-two-numbers.swift
+++ swift/add-two-numbers.swift
@@ -1,5 +1,3 @@
-//: [Previous](@previous)
-
 // https://programmers.co.kr/learn/courses/30/lessons/68644
 
 /**
@@ -26,4 +24,3 @@ func solution(_ numbers:[Int]) -> [Int] {
 print(solution([2,1,3,4,1])) // [2,3,4,5,6,7]
 print(solution([5,0,2,7])) // [2,5,7,9,12]
 
-//: [Next](@next)
