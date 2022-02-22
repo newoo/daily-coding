aaf81332b32f626883fccfb1cc5b253c9e6c0b04
newoo <newoo4297@naver.com>
2022-02-22 22:50:27 +0900

    Solve crane-puppet-game by swift

--- swift/crane-puppet-game-20220222.swift
+++ swift/crane-puppet-game-20220222.swift
@@ -1,5 +1,3 @@
-//: [Previous](@previous)
-
 // https://programmers.co.kr/learn/courses/30/lessons/64061
 
 /**
@@ -43,4 +41,3 @@ func relocate(board: [[Int]]) -> [[Int]] {
 
 print(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4])) // 4
 
-//: [Next](@next)
