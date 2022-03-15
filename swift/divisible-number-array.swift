557418039bb7bbb02de3016137fd37b50058f2ae
newoo <newoo4297@naver.com>
2022-03-15 23:25:00 +0900

    Solve divisible-number-array by swift

--- swift/divisible-number-array.swift
+++ swift/divisible-number-array.swift
@@ -1,5 +1,3 @@
-//: [Previous](@previous)
-
 // https://programmers.co.kr/learn/courses/30/lessons/12910
 
 /**
@@ -16,4 +14,3 @@ print(solution([5, 9, 7, 10], 5)) // [5, 10]
 print(solution([2, 36, 1, 3], 1)) // [1, 2, 3, 36]
 print(solution([3,2,6], 10)) // [-1]
 
-//: [Next](@next)
