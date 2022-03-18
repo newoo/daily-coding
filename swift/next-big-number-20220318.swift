02565521ffe1376f9dd5a4c51b1f32ce238afcd2
newoo <newoo4297@naver.com>
2022-03-18 23:29:51 +0900

    Fail to solve next-big-number by swift

--- swift/next-big-number-20220318.swift
+++ swift/next-big-number-20220318.swift
@@ -1,5 +1,3 @@
-//: [Previous](@previous)
-
 // https://programmers.co.kr/learn/courses/30/lessons/12911
 
 /**
@@ -20,4 +18,3 @@ func solution(_ n:Int) -> Int {
 print(solution(78)) // 83
 print(solution(15)) // 23
 
-//: [Next](@next)
