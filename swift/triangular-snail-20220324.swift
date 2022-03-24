efa8717934c398b523f7c94a1995e59695614bf5
newoo <newoo4297@naver.com>
2022-03-24 22:43:24 +0900

    Solve triangular-snail by swift

--- swift/triangular-snail-20220324.swift
+++ swift/triangular-snail-20220324.swift
@@ -1,5 +1,3 @@
-//: [Previous](@previous)
-
 // https://programmers.co.kr/learn/courses/30/lessons/68645
 
 /**
@@ -51,4 +49,3 @@ print(solution(4)) // [1,2,9,3,10,8,4,5,6,7]
 print(solution(5)) // [1,2,12,3,13,11,4,14,15,10,5,6,7,8,9]
 print(solution(6)) // [1,2,15,3,16,14,4,17,21,13,5,18,19,20,12,6,7,8,9,10,11]
 
-//: [Next](@next)
