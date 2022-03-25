2d900b5ff56280eee4f4b04eade5d21905b5c701
newoo <newoo4297@naver.com>
2022-03-25 23:48:19 +0900

    Fail to solve land-conquest by swift

--- swift/land-conquest-20220325.swift
+++ swift/land-conquest-20220325.swift
@@ -1,5 +1,3 @@
-//: [Previous](@previous)
-
 // https://programmers.co.kr/learn/courses/30/lessons/12913
 
 /**
@@ -29,4 +27,3 @@ func step(current: [Int], next: [[Int]]) -> [Int] {
 
 print(solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]])) // 16
 
-//: [Next](@next)
