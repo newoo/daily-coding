ffaa2237422798659dd8da30396b26a01375c618
newoo <newoo4297@naver.com>
2022-04-06 02:35:18 +0900

    Fail to solve gem-shopping by swift again

--- swift/gem-shopping-20220406-2.swift
+++ swift/gem-shopping-20220406-2.swift
@@ -1,29 +1,29 @@
-//: [Previous](@previous)
-
 // https://programmers.co.kr/learn/courses/30/lessons/67258
 
 /**
- 효율성 테스트 몇 개 통과 못 함
  */
 
 import Foundation
 
 func solution(_ gems:[String]) -> [Int] {
-  let count = Set(gems).count
-  var dic = [String: Int]()
-  
-  var arr = [[Int]]()
-  
+  var dic = Dictionary(uniqueKeysWithValues: Set(gems).map { ($0, [Int]()) })
+
   gems.enumerated().forEach {
-    dic[$0.element] = $0.offset + 1
-    
-    if dic.count == count {
-      let values = dic.values
-      arr.append([values.min()!, values.max()!])
-    }
+    dic[$0.element] = dic[$0.element, default: []] + [$0.offset]
   }
-  
-  return arr.min(by: { ($0.last! - $0.first!) < ($1.last! - $1.first!) }) ?? []
+
+  var _dic = Dictionary(uniqueKeysWithValues: dic.map { ($0.key, ($0.value.min()!, $0.value.max()!)) })
+
+  print(_dic)
+
+  let r = _dic.reduce([0, gems.endIndex]) {
+
+    print("min max", _min, _max)
+
+    return [_min, _max]
+  }
+
+  return []
 }
 
 print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"])) // [3, 7]
@@ -31,4 +31,3 @@ print(solution(["AA", "AB", "AC", "AA", "AC"])) // [1, 3]
 print(solution(["XYZ", "XYZ", "XYZ"])) // [1, 1]
 print(solution(["ZZZ", "YYY", "NNNN", "YYY", "BBB"])) // [1, 5]
 
-//: [Next](@next)
--- swift/gem-shopping-20220406-3.swift
+++ swift/gem-shopping-20220406-3.swift
@@ -1,9 +1,6 @@
-//: [Previous](@previous)
-
 // https://programmers.co.kr/learn/courses/30/lessons/67258
 
 /**
- 효율성 테스트 몇 개 통과 못 함
  */
 
 import Foundation
@@ -11,19 +8,18 @@ import Foundation
 func solution(_ gems:[String]) -> [Int] {
   let count = Set(gems).count
   var dic = [String: Int]()
-  
-  var arr = [[Int]]()
-  
-  gems.enumerated().forEach {
-    dic[$0.element] = $0.offset + 1
-    
+
+  for (offset, gem) in gems.enumerated() {
     if dic.count == count {
-      let values = dic.values
-      arr.append([values.min()!, values.max()!])
+      break
     }
+
+    dic[gem] = offset + 1
   }
-  
-  return arr.min(by: { ($0.last! - $0.first!) < ($1.last! - $1.first!) }) ?? []
+
+  let values = dic.values
+
+  return [values.min()!, values.max()!]
 }
 
 print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"])) // [3, 7]
@@ -31,4 +27,3 @@ print(solution(["AA", "AB", "AC", "AA", "AC"])) // [1, 3]
 print(solution(["XYZ", "XYZ", "XYZ"])) // [1, 1]
 print(solution(["ZZZ", "YYY", "NNNN", "YYY", "BBB"])) // [1, 5]
 
-//: [Next](@next)
